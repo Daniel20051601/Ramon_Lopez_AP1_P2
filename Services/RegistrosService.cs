1aac3cd63a9f0da3d8e1bbe724e39c2687b56bb7
Danielvi <danielvilopez@gmail.com>
2025-07-07 18:50:50 -0400

    Crea la clase servicio

---
+++ Services/RegistrosService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ramon_Lopez_AP1_P2.Dal;
+
+namespace Ramon_Lopez_AP1_P2.Services;
+
+public class RegistrosService(IDbContextFactory<Contexto> DbContext)
+{
+
+}
