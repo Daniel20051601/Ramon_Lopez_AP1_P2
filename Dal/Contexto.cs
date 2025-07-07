0681d0e51d25c64d67c441f33b5790e057abfb32
Danielvi <danielvilopez@gmail.com>
2025-07-07 18:51:39 -0400

    Crea la clase contexto para manejar la base de datos

---
+++ Dal/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ramon_Lopez_AP1_P2.Dal;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+    public Contexto() { }
+
+    public DbSet<Models.Registro> Registros { get; set; } = null!;
+    
+}
