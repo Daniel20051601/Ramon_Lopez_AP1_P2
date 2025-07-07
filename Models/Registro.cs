0a8965f90e079561c037ee081b12ecd675516ace
Danielvi <danielvilopez@gmail.com>
2025-07-07 18:51:12 -0400

    Crea la clase modelo

---
+++ Models/Registro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Lopez_AP1_P2.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int RegistroId { get; set; }
+    }
+}
