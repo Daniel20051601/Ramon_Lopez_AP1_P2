bf9d6714d47a2f09a44171aabc859ef7d1ffa720
Danielvi <danielvilopez@gmail.com>
2025-07-14 19:23:53 -0400

    Crea los modelos para las diferentes tablas

---
+++ Models/Entradas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ramon_Lopez_AP1_P2.Models;
+
+public class Entradas
+{
+    [Key]
+    public int EntradaId { get; set; }
+
+    public DateTime Fecha { get; set; } = DateTime.UtcNow;
+
+    [Required(ErrorMessage = "Debe agregar un concepto")]
+    public string Concepto { get; set; } = null!;
+
+    [InverseProperty("Entradas")]
+    public virtual ICollection<EntradasDetalle> EntradasDetalle { get; set; } = new List<EntradasDetalle>();
+}
---
+++ Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Lopez_AP1_P2.Models;
+
+public class Productos
+{
+    [Key]
+    public int ProductoId { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public decimal Peso { get; set; }
+
+    public decimal Existencia { get; set; }
+
+    public bool EsCompuesto { get; set; }
+
+    public virtual ICollection<EntradasDetalle> EntradasDetalles { get; set; } = new List<EntradasDetalle>();
+}
