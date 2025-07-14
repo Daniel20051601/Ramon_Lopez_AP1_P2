3246fab57f66bb15cf4beeded0d2ba6cb5cb25b6
Danielvi <danielvilopez@gmail.com>
2025-07-14 19:24:31 -0400

    Agrega las tablas al contexto y agrega algunos productos

--- Dal/Contexto.cs
+++ Dal/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ramon_Lopez_AP1_P2.Models;
 
 namespace Ramon_Lopez_AP1_P2.Dal;
 
@@ -10,6 +11,19 @@ public class Contexto : DbContext
 
     public Contexto() { }
 
-    public DbSet<Models.Registro> Registros { get; set; } = null!;
-    
+    public DbSet<Productos> Productos { get; set; } = null!;
+    public DbSet<Entradas> Entradas { get; set; } = null!;
+    public DbSet<EntradasDetalle> EntradasDetalle { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Productos>().HasData(
+            new Productos {ProductoId = 1, Descripcion = "Maní",Peso = 5.00m,Existencia = 100.0m, EsCompuesto = false},
+            new Productos {ProductoId = 2, Descripcion = "Almendra", Peso = 10.00m, Existencia = 75.0m, EsCompuesto = false },
+            new Productos {ProductoId = 3, Descripcion = "Pistacho", Peso = 20.00m, Existencia = 50.0m, EsCompuesto = false}
+        );
+    }
+
 }
